7a2bc8c088b960ce8e52970f26132b6ea0b0072a
Daniclase <ihavenomoneyforgacha@gmail.com>
2024-12-10 20:52:12 +0100

    calculator finished

--- Calculadora/app.js
+++ Calculadora/app.js
@@ -41,6 +41,12 @@ window.onload = function() {
 
 const inputDisplay = document.getElementById('numDisplay');
 const subDisplay = document.getElementById('operDisplay');
+const num_buttons = document.querySelectorAll('.calcButN');
+const op_buttons = document.querySelectorAll('.calcButO');
+const dot_button = document.querySelectorAll('.calcButD');
+const del_button = document.querySelectorAll('.calcButB');
+const res_button = document.querySelectorAll('.calcButR');
+const eq_button = document.querySelectorAll('.calcButE');
 let num1 = '';
 let num2 = '';
 let operator = '';
@@ -48,13 +54,22 @@ let result = '';
 let opIndex = 0;
 let history = new History(0);
 
+op_buttons.forEach(button => button.disabled = true);
+eq_button.forEach(button => button.disabled = true);
+del_button.forEach(button => button.disabled = true);
+
 function addNumber(num) {
     if (operator === '') {
         num1 += num;
         inputDisplay.value = num1;
+        op_buttons.forEach(button => button.disabled = false);
+        del_button.forEach(button => button.disabled = false);
     } else {
         num2 += num;
         inputDisplay.value = num2;
+        op_buttons.forEach(button => button.disabled = false);
+        eq_button.forEach(button => button.disabled = false);
+        del_button.forEach(button => button.disabled = false);
     }
 }
 
@@ -62,7 +77,10 @@ function addOperator(op) {
     if (inputDisplay.value === '0') {
         return;
     } else if (operator === '') {
-        subDisplay.value = inputDisplay.value + ' ' + op;
+        subDisplay.value = num1 + ' ' + op;
+        inputDisplay.value = '0';
+        op_buttons.forEach(button => button.disabled = true);
+        dot_button.forEach(button => button.disabled = false);
     } else {
         calculate();
         subDisplay.value = result + ' ' + op;
@@ -75,6 +93,8 @@ function addOperator(op) {
 function equals() {
     calculate();
     subDisplay.value = '';
+    del_button.forEach(button => button.disabled = true);
+    dot_button.forEach(button => button.disabled = true);
 }
 
 function calculate() {
@@ -95,18 +115,24 @@ function calculate() {
     inputDisplay.value = result;
     history.addOperation(num1, num2, operator, result, opIndex);
     updateHistoryList();
-    num1 = '';
+    num1 = result;
     num2 = '';
     operator = '';
     opIndex++;
 }
 
 function reset() {
-    inputDisplay.value = '0';
+location.reload();
+   /* inputDisplay.value = '0';
     subDisplay.value = '';
     num1 = '';
     num2 = '';
     operator = '';
+    result = '';
+    op_buttons.forEach(button => button.disabled = true);
+    del_button.forEach(button => button.disabled = true);
+    eq_button.forEach(button => button.disabled = true);
+    */
 }
 
 function dot(){
@@ -114,15 +140,23 @@ function dot(){
         if (!num1.includes('.')) {
             num1 += '.';
             inputDisplay.value = num1;
+            dot_button.forEach(button => button.disabled = true);
         }
     } else {
         if (!num2.includes('.')) {
             num2 += '.';
             inputDisplay.value = num2;
+            dot_button.forEach(button => button.disabled = true);
         }
     }
     }
 
-
-
-
+function del() {
+    if (operator === '') {
+        num1 = num1.slice(0, -1);
+        inputDisplay.value = num1;
+    } else {
+        num2 = num2.slice(0, -1);
+        inputDisplay.value = num2;
+    }
+}
